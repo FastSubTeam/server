39a833640ee702bc674dd17c64d60d6f7c7298ce
khsrla9806 <kimhunsope@naver.com>
2023-07-06 18:39:57 +0900

    [#8] feat: OptionCreateDto 생성

---
+++ src/main/java/com/popple/server/domain/survey/dto/OptionCreateDto.java
@@ -0,0 +1,14 @@
+package com.popple.server.domain.survey.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OptionCreateDto {
+    private String content;
+}
