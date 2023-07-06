e052d6a1fbd6d9f8a162f0ade746d9047c989874
khsrla9806 <kimhunsope@naver.com>
2023-07-06 18:37:04 +0900

    [#8] refactor: SurveyOption Entity 클래스 코드 수정
    - 필요한 Lombok 코드 추가

--- src/main/java/com/popple/server/domain/entity/SurveyOption.java
+++ src/main/java/com/popple/server/domain/entity/SurveyOption.java
@@ -1,8 +1,17 @@
 package com.popple.server.domain.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.sql.Timestamp;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
 @Entity
 public class SurveyOption {
     @Id
