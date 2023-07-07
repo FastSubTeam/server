fbc75e1a3d4e1d7cf0bf52c06fab7a9c1066ea85
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-07-07 13:29:50 +0900

    [#1] feat: User Entity 디테일한 설정 추가
    모든 필드에 NotNull 설정
    profileImgUrl 필드의 default 값은 추후 논의 후 변경 필요
    inactive 필드는 삭제시 true로 값 변경

--- src/main/java/com/popple/server/domain/entity/User.java
+++ src/main/java/com/popple/server/domain/entity/User.java
@@ -1,9 +1,13 @@
 package com.popple.server.domain.entity;
 
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 import java.sql.Timestamp;
 
 @Entity
@@ -11,12 +15,33 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
     private String email;
+
+    @NotNull
     private String password;
+
+    @NotNull
     private String city;
+
+    @NotNull
     private String district;
+
+    @NotNull
     private String nickname;
+
+
+    //TODO 이후 디폴트 이미지를 정하고, 해당 이미지의 주소로 디폴트 값 변경 필요
+    @ColumnDefault(value = "'profileDefaultImageUrl'")
+    @NotNull
     private String profileImgUrl;
+
+    @ColumnDefault(value = "false")
+    @NotNull
     private Boolean inactive;
+
+    @CreatedDate
+    @NotNull
     private Timestamp createdAt;
 }
