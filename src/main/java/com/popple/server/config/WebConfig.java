33d249d316b9fe2bed25eaf6a69c2430ea315599
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-07-10 19:25:34 +0900

    [#1] feat: 쿼리 파리미터로 받는 Role값에 대한 컨버터 구현

---
+++ src/main/java/com/popple/server/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.popple.server.config;
+
+import com.popple.server.common.converter.RoleRequestConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new RoleRequestConverter());
+    }
+}
