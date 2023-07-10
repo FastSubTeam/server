f622926d30c435cfb8311f05deb261a2e395173a
khsrla9806 <kimhunsope@naver.com>
2023-07-11 00:45:13 +0900

    [#22] test: 관리자 수요조사 등록 테스트 리펙토링
    - 반복되는 mvc.perform() 코드를 메서드로 분리했습니다.

--- src/test/java/com/popple/server/domain/survey/controller/SurveyAdminControllerTest.java
+++ src/test/java/com/popple/server/domain/survey/controller/SurveyAdminControllerTest.java
@@ -36,26 +36,17 @@ class SurveyAdminControllerTest {
     @DisplayName("[POST] 관리자 수요조사 등록 성공")
     void createSurveySuccess() throws Exception {
         // Given
+        String title = "12월에 받고 싶은 선물은?";
         Timestamp startDate = Timestamp.valueOf(LocalDateTime.now().plusDays(1L));
         Timestamp endDate = Timestamp.valueOf(LocalDateTime.now().plusMonths(1L));
-        SurveyCreateReqDto dto = new SurveyCreateReqDto();
-        dto.setTitle("12월에 받고 싶은 선물은?");
-        dto.setStartDate(startDate);
-        dto.setEndDate(endDate);
         List<OptionCreateDto> options = List.of(
                 new OptionCreateDto("산타인형"),
                 new OptionCreateDto("닌텐도"),
                 new OptionCreateDto("트리")
         );
-        dto.setOptions(options);
-
-        String jsonData = om.writeValueAsString(dto);
 
         // When
-        ResultActions resultActions = mvc.perform(post("/api/admin/survey")
-                .contentType(MediaType.APPLICATION_JSON)
-                .characterEncoding(StandardCharsets.UTF_8)
-                .content(jsonData));
+        ResultActions resultActions = requestCreateSurvey(title, startDate, endDate, options);
 
         // Then
         resultActions.andExpect(jsonPath("$.statusCode", HttpStatus.CREATED.value()).exists());
@@ -65,25 +56,17 @@ class SurveyAdminControllerTest {
     @DisplayName("[POST] 관리자 수요조사 등록 실패 - 오늘보다 과거 날짜 선택")
     void createSurveyFailWithPastThanToday() throws Exception {
         // Given
-        Timestamp yesterday = Timestamp.valueOf(LocalDateTime.now().minusDays(1L));
-        SurveyCreateReqDto dto = new SurveyCreateReqDto();
-        dto.setTitle("12월에 받고 싶은 선물은?");
-        dto.setStartDate(yesterday);
-        dto.setEndDate(yesterday);
+        String title = "12월에 받고 싶은 선물은?";
+        Timestamp startDate = Timestamp.valueOf(LocalDateTime.now().minusDays(2L));
+        Timestamp endDate = Timestamp.valueOf(LocalDateTime.now().minusDays(1L));
         List<OptionCreateDto> options = List.of(
                 new OptionCreateDto("산타인형"),
                 new OptionCreateDto("닌텐도"),
                 new OptionCreateDto("트리")
         );
-        dto.setOptions(options);
-
-        String jsonData = om.writeValueAsString(dto);
 
         // When
-        ResultActions resultActions = mvc.perform(post("/api/admin/survey")
-                .contentType(MediaType.APPLICATION_JSON)
-                .characterEncoding(StandardCharsets.UTF_8)
-                .content(jsonData));
+        ResultActions resultActions = requestCreateSurvey(title, startDate, endDate, options);
 
         // Then
         String errorMessage = "유효성 검사 실패";
@@ -95,26 +78,17 @@ class SurveyAdminControllerTest {
     @DisplayName("[POST] 관리자 수요조사 등록 실패 - Title Blank")
     void createSurveyFailWithBlankTitle() throws Exception {
         // Given
+        String title = "";
         Timestamp startDate = Timestamp.valueOf(LocalDateTime.now().plusDays(1L));
         Timestamp endDate = Timestamp.valueOf(LocalDateTime.now().plusMonths(1L));
-        SurveyCreateReqDto dto = new SurveyCreateReqDto();
-        dto.setTitle("");
-        dto.setStartDate(startDate);
-        dto.setEndDate(endDate);
         List<OptionCreateDto> options = List.of(
                 new OptionCreateDto("산타인형"),
                 new OptionCreateDto("닌텐도"),
                 new OptionCreateDto("트리")
         );
-        dto.setOptions(options);
-
-        String jsonData = om.writeValueAsString(dto);
 
         // When
-        ResultActions resultActions = mvc.perform(post("/api/admin/survey")
-                .contentType(MediaType.APPLICATION_JSON)
-                .characterEncoding(StandardCharsets.UTF_8)
-                .content(jsonData));
+        ResultActions resultActions = requestCreateSurvey(title, startDate, endDate, options);
 
         // Then
         String errorMessage = "유효성 검사 실패";
@@ -126,24 +100,15 @@ class SurveyAdminControllerTest {
     @DisplayName("[POST] 관리자 수요조사 등록 실패 - option 1개 선택")
     void createSurveyFailWithMinOptionsSize() throws Exception {
         // Given
+        String title = "12월에 받고 싶은 선물은?";
         Timestamp startDate = Timestamp.valueOf(LocalDateTime.now().plusDays(1L));
         Timestamp endDate = Timestamp.valueOf(LocalDateTime.now().plusMonths(1L));
-        SurveyCreateReqDto dto = new SurveyCreateReqDto();
-        dto.setTitle("12월에 받고 싶은 선물은?");
-        dto.setStartDate(startDate);
-        dto.setEndDate(endDate);
         List<OptionCreateDto> options = List.of(
                 new OptionCreateDto("산타인형")
         );
-        dto.setOptions(options);
-
-        String jsonData = om.writeValueAsString(dto);
 
         // When
-        ResultActions resultActions = mvc.perform(post("/api/admin/survey")
-                .contentType(MediaType.APPLICATION_JSON)
-                .characterEncoding(StandardCharsets.UTF_8)
-                .content(jsonData));
+        ResultActions resultActions = requestCreateSurvey(title, startDate, endDate, options);
 
         // Then
         String errorMessage = "유효성 검사 실패";
@@ -155,25 +120,16 @@ class SurveyAdminControllerTest {
     @DisplayName("[POST] 관리자 수요조사 등록 실패 - endDate가 startDate와 같은 경우")
     void createSurveyFailWithEndDateEqualsStartDate() throws Exception {
         // Given
+        String title = "12월에 받고 싶은 선물은?";
         Timestamp date = Timestamp.valueOf(LocalDateTime.now().plusDays(1L));
-        SurveyCreateReqDto dto = new SurveyCreateReqDto();
-        dto.setTitle("12월에 받고 싶은 선물은?");
-        dto.setStartDate(date);
-        dto.setEndDate(date);
         List<OptionCreateDto> options = List.of(
                 new OptionCreateDto("산타인형"),
                 new OptionCreateDto("닌텐도"),
                 new OptionCreateDto("트리")
         );
-        dto.setOptions(options);
-
-        String jsonData = om.writeValueAsString(dto);
 
         // When
-        ResultActions resultActions = mvc.perform(post("/api/admin/survey")
-                .contentType(MediaType.APPLICATION_JSON)
-                .characterEncoding(StandardCharsets.UTF_8)
-                .content(jsonData));
+        ResultActions resultActions = requestCreateSurvey(title, date, date, options);
 
         // Then
         String errorMessage = "수요조사 종료 날짜가 시작 날짜와 같거나 과거일 수 없습니다.";
@@ -185,30 +141,38 @@ class SurveyAdminControllerTest {
     @DisplayName("[POST] 관리자 수요조사 등록 실패 - endDate가 startDate 보다 과거 일 경우")
     void createSurveyFailWithEndDateBeforeStartDate() throws Exception {
         // Given
+        String title = "12월에 받고 싶은 선물은?";
         Timestamp startDate = Timestamp.valueOf(LocalDateTime.now().plusDays(3L));
         Timestamp endDate = Timestamp.valueOf(LocalDateTime.now().plusDays(2L));
-        SurveyCreateReqDto dto = new SurveyCreateReqDto();
-        dto.setTitle("12월에 받고 싶은 선물은?");
-        dto.setStartDate(startDate);
-        dto.setEndDate(endDate);
         List<OptionCreateDto> options = List.of(
                 new OptionCreateDto("산타인형"),
                 new OptionCreateDto("닌텐도"),
                 new OptionCreateDto("트리")
         );
-        dto.setOptions(options);
-
-        String jsonData = om.writeValueAsString(dto);
 
         // When
-        ResultActions resultActions = mvc.perform(post("/api/admin/survey")
-                .contentType(MediaType.APPLICATION_JSON)
-                .characterEncoding(StandardCharsets.UTF_8)
-                .content(jsonData));
+        ResultActions resultActions = requestCreateSurvey(title, startDate, endDate, options);
 
         // Then
         String errorMessage = "수요조사 종료 날짜가 시작 날짜와 같거나 과거일 수 없습니다.";
         resultActions.andExpect(jsonPath("$.errorCode", HttpStatus.BAD_REQUEST.value()).exists());
         resultActions.andExpect(jsonPath("$.message", errorMessage).exists());
+    }
+
+    private ResultActions requestCreateSurvey(
+            String title, Timestamp startDate, Timestamp endDate, List<OptionCreateDto> options) throws Exception {
+
+        SurveyCreateReqDto dto = new SurveyCreateReqDto();
+        dto.setTitle(title);
+        dto.setStartDate(startDate);
+        dto.setEndDate(endDate);
+        dto.setOptions(options);
+
+        String jsonData = om.writeValueAsString(dto);
+
+        return mvc.perform(post("/api/admin/survey")
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding(StandardCharsets.UTF_8)
+                .content(jsonData));
     }
 }
