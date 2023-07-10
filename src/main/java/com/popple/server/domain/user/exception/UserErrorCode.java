24d85a114abde58e5bab9f7ce215dba5d6620c25
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-07-10 19:37:41 +0900

    [#1] feat: User 도메인에서 사용하는 에러 메시지 및 레포지토리 메소드 추가

--- src/main/java/com/popple/server/domain/user/exception/UserErrorCode.java
+++ src/main/java/com/popple/server/domain/user/exception/UserErrorCode.java
@@ -6,7 +6,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum UserErrorCode implements ErrorCode {
 
-    EXIST_EMAIL(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일입니다.");
+    EXIST_EMAIL(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일입니다."),
+    PROCEEDING_EMAIL(HttpStatus.BAD_REQUEST, "이미 회원가입을 진행중인 이메일입니다"),
+    NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 회원입니다"),
+    ;
 
     private final HttpStatus httpStatus;
     private final String errorMessage;
@@ -15,4 +18,4 @@ public enum UserErrorCode implements ErrorCode {
         this.httpStatus = httpStatus;
         this.errorMessage = errorMessage;
     }
-}
+    }
--- src/main/java/com/popple/server/domain/user/repository/UserRepository.java
+++ src/main/java/com/popple/server/domain/user/repository/UserRepository.java
@@ -1,8 +1,14 @@
-package com.popple.server.domain.user;
+package com.popple.server.domain.user.repository;
 
 import com.popple.server.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
 }
