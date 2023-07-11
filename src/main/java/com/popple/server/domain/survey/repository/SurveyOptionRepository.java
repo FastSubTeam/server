1b57e4ffbb08466ebe5461fce6c91fec2a6fc341
khsrla9806 <kimhunsope@naver.com>
2023-07-11 23:44:22 +0900

    [#24] feat: SurveyOptionRepository에서 SurveyId로 Select하는 메서드 작성
    - Survey와 ManyToOne 관계이기 때문에 List로 반환하도록 했습니다.

--- src/main/java/com/popple/server/domain/survey/repository/SurveyOptionRepository.java
+++ src/main/java/com/popple/server/domain/survey/repository/SurveyOptionRepository.java
@@ -3,5 +3,8 @@ package com.popple.server.domain.survey.repository;
 import com.popple.server.domain.entity.SurveyOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SurveyOptionRepository extends JpaRepository<SurveyOption, Integer> {
+    List<SurveyOption> findBySurveyId(int surveyId);
 }
