3d39f3187c369dd6962e850ad458b80eceade661
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-07-12 05:09:00 +0900

    [#13] feat: RegisterToken 인증 위한 Dto 수정
    정확한 인증을 위해 Email 필드 추가

--- src/main/java/com/popple/server/domain/user/dto/VerifyEmailRequestDto.java
+++ src/main/java/com/popple/server/domain/user/dto/VerifyEmailRequestDto.java
@@ -2,8 +2,19 @@ package com.popple.server.domain.user.dto;
 
 import lombok.Getter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Getter
 public class VerifyEmailRequestDto {
 
+    @NotNull
+    @NotEmpty
+    @Email
+    private String email;
+
+    @NotEmpty
+    @NotNull
     private String registerToken;
 }
