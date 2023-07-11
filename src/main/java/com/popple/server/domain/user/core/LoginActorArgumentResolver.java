6382d16c609e6872aa1dc0d562b3a888f4f6b3b6
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-07-12 05:10:03 +0900

    [#13] feat: LoginActorArgumentResolver 수정
    email 필드 제거

--- src/main/java/com/popple/server/domain/user/core/LoginActorArgumentResolver.java
+++ src/main/java/com/popple/server/domain/user/core/LoginActorArgumentResolver.java
@@ -5,10 +5,14 @@ import com.popple.server.domain.user.jwt.TokenManager;
 import com.popple.server.domain.user.service.TokenService;
 import com.popple.server.domain.user.vo.Actor;
 import com.popple.server.domain.user.vo.Role;
+import com.popple.server.domain.user.vo.Token;
 import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -17,6 +21,9 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -32,20 +39,15 @@ public class LoginActorArgumentResolver implements HandlerMethodArgumentResolver
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        String token = tokenManager.extractBearerToken(((ServletRequestAttributes) webRequest).getRequest());
-        if (token == null || !tokenManager.validateToken(token)) {
-            throw new RuntimeException();
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null) {
+            throw new RuntimeException("인증 정보가 존재하지 않습니다");
         }
-
-        Claims parseClaims = tokenManager.parseClaims(token);
-        Long id = parseClaims.get("id", Long.class);
-        String email = parseClaims.get("email", String.class);
-        Role role = Role.of(parseClaims.get("role").toString());
-
-
+        Long id = Long.parseLong(authentication.getName());
+        List<? extends GrantedAuthority> authorities = (List) authentication.getAuthorities();
+        Role role = Role.of(authorities.get(0).toString());
         return Actor.builder()
                 .id(id)
-                .email(email)
                 .role(role)
                 .build();
     }
--- src/main/java/com/popple/server/domain/user/vo/Actor.java
+++ src/main/java/com/popple/server/domain/user/vo/Actor.java
@@ -8,6 +8,5 @@ import lombok.Getter;
 public class Actor {
 
     private Long id;
-    private String email;
     private Role role;
 }
