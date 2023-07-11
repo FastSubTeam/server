9bf82e5fb98b0e17dfabec6b9502f0b1719a2ea0
brotherstone97 <brotherstone97@gmail.com>
2023-07-11 15:35:44 +0900

    [#20] feat: BoardListRespDto 생성

---
+++ src/main/java/com/popple/server/domain/board/dto/BoardListRespDto.java
@@ -0,0 +1,22 @@
+package com.popple.server.domain.board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+//@Todo 전체 리스트 조회 구현 후 페이지네이션 구현
+public class BoardListRespDto {
+    private String nickname;
+    private String title;
+    private String content;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+    private int commentCount;
+}
