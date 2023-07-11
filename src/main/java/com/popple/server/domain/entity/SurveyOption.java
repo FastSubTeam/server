8921b92f6cdbce8dd5f896594ca6a3e43f89ee70
khsrla9806 <kimhunsope@naver.com>
2023-07-12 01:22:05 +0900

    [#25] refactor: Survey, SurveyOption에서 수정을 요구하는 필드에 Setter 추가
    - Survey는 수정 가능한 title, startDate, endDate, status에 추가
    - SurveyOption은 수정 가능한 content에 추가

--- src/main/java/com/popple/server/domain/entity/SurveyOption.java
+++ src/main/java/com/popple/server/domain/entity/SurveyOption.java
@@ -1,9 +1,6 @@
 package com.popple.server.domain.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
@@ -21,6 +18,7 @@ public class SurveyOption {
     @ManyToOne(fetch = FetchType.LAZY)
     private Survey survey;
 
+    @Setter
     @Column(nullable = false, length = 300)
     private String content;
 
