1de09202d9fcc970ba73c65e865c26653f8b7d0d
brotherstone97 <brotherstone97@gmail.com>
2023-07-11 16:44:55 +0900

    [#20]feat: 모든 게시물 및 포스트별 댓글 조회 서비스 로직 추가
    포스트별 댓글 조회는 postId를 받아 repository 메서드를 호출하도록 했습니다.

--- src/main/java/com/popple/server/domain/board/service/BoardService.java
+++ src/main/java/com/popple/server/domain/board/service/BoardService.java
@@ -1,11 +1,25 @@
 package com.popple.server.domain.board.service;
 
 import com.popple.server.domain.board.repository.BoardRepository;
+import com.popple.server.domain.board.repository.CommentRepository;
+import com.popple.server.domain.entity.Comment;
+import com.popple.server.domain.entity.Post;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class BoardService {
     private final BoardRepository boardRepository;
+    private final CommentRepository commentRepository;
+
+    public List<Post> getAllPosts() {
+        return boardRepository.findAll();
+    }
+
+    public List<Comment> getAllCommentsByPostId(Long postId){
+        return commentRepository.findByPost_Id(postId);
+    }
 }
