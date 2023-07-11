fc427b78d35acb3baab3fadec099f71c2f2bb4e0
brotherstone97 <brotherstone97@gmail.com>
2023-07-11 15:23:15 +0900

    [#20] feat: Post, Comment 엔티티 제약조건 추가

--- src/main/java/com/popple/server/domain/entity/Post.java
+++ src/main/java/com/popple/server/domain/entity/Post.java
@@ -1,19 +1,40 @@
 package com.popple.server.domain.entity;
 
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.sql.Timestamp;
 
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "user_id")
+    @NotNull
     private Member member;
+
+    @NotBlank
+    @Size(max = 50)
     private String title;
+
+    @NotBlank
     private String content;
+
+    @Column(updatable = false)
+    @CreationTimestamp
     private Timestamp createdAt;
+
+    @UpdateTimestamp
     private Timestamp updatedAt;
 }
