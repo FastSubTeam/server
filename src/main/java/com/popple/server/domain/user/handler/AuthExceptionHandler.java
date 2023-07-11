58c41acd0dbd14e9703997150f6d4458ceaf7af0
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-07-12 05:06:56 +0900

    [#13] feat: AuthExceptionHandler 추가 구현
    자바 빈 validation 예외에 대한 추가 처리 구현 ConstraintViolationException, MethodArgumentNotValidException,
    처리하지 못한 예외 처리할 수 있도록 RuntimeExceptionHandler 구현

--- src/main/java/com/popple/server/domain/user/handler/AuthExceptionHandler.java
+++ src/main/java/com/popple/server/domain/user/handler/AuthExceptionHandler.java
@@ -3,17 +3,45 @@ package com.popple.server.domain.user.handler;
 import com.popple.server.common.dto.APIErrorResponse;
 import com.popple.server.domain.user.controller.AuthController;
 import com.popple.server.domain.user.exception.AlreadyExistException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.validation.ConstraintViolationException;
+import java.util.Objects;
+
+@Slf4j
 @RestControllerAdvice(basePackageClasses = {AuthController.class})
 public class AuthExceptionHandler {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public APIErrorResponse constraintViolationExceptionHandle(ConstraintViolationException e) {
+        return APIErrorResponse.of(HttpStatus.BAD_REQUEST, e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public APIErrorResponse methodArgumentNotValidExceptionHandle(MethodArgumentNotValidException e) {
+
+        // TODO 수정 필요, APIErrorResponse를 바꾸기
+        return APIErrorResponse.of(HttpStatus.BAD_REQUEST, Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage());
+    }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public APIErrorResponse alreadyExistExceptionHandle(AlreadyExistException e) {
         return APIErrorResponse.of(HttpStatus.BAD_REQUEST, e.getMessage());
     }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public APIErrorResponse unknownExceptionHandle(RuntimeException e) {
+        log.error(e.toString());
+        return APIErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+    }
+
 }
