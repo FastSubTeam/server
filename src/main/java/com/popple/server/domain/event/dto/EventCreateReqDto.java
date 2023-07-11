036e07ab016f4f37a4d83d3b763aa95638e51d1d
WooSeok77 <wi8tkf@naver.com>
2023-07-11 16:17:00 +0900

    [#17] feat: EventCreateReqDto 구현

---
+++ src/main/java/com/popple/server/domain/event/dto/EventCreateReqDto.java
@@ -0,0 +1,24 @@
+package com.popple.server.domain.event.dto;
+
+
+import com.popple.server.domain.entity.Seller;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class EventCreateReqDto {
+    private String name;
+    private String description;
+    private String location;
+    private String thumbnailUrl;
+    private Timestamp startDate;
+    private Timestamp endDate;
+
+}
