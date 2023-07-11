fc232b71a3b37bfd8bce7cd2e0dc43abe1f138cb
WooSeok77 <wi8tkf@naver.com>
2023-07-11 16:49:17 +0900

    [#17] feat: EventService(save), EventApproval Enum클래스 구현

---
+++ src/main/java/com/popple/server/domain/event/service/EventService.java
@@ -0,0 +1,38 @@
+package com.popple.server.domain.event.service;
+
+import com.popple.server.domain.entity.Event;
+import com.popple.server.domain.event.dto.EventCreateReqDto;
+import com.popple.server.domain.event.repository.EventRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+import static com.popple.server.domain.event.EventApproval.WAIT;
+@RequiredArgsConstructor
+@Service
+public class EventService {
+    private final EventRepository eventRepository;
+
+    @Transactional
+    public Event save(EventCreateReqDto dto) {
+        Event event = Event.builder()
+                .name(dto.getName())
+                .description(dto.getDescription())
+                .location(dto.getLocation())
+                .thumbnailUrl(dto.getThumbnailUrl())
+                .startDate(dto.getStartDate())
+                .endDate(dto.getEndDate())
+                .approval(WAIT)
+                .createdAt(Timestamp.valueOf(LocalDateTime.now()))
+                .build();
+
+        eventRepository.save(event);
+
+        return event;
+    }
+
+
+}
