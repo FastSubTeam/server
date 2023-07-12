e25c4484f245373015abdc8ac81f36283b38ab2e
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-07-12 16:40:50 +0900

    [#13] fix: origin 모두 허용하도록 SecurityConfig 파일 수정

--- src/main/java/com/popple/server/config/SecurityConfig.java
+++ src/main/java/com/popple/server/config/SecurityConfig.java
@@ -75,7 +75,8 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*"); // 모든 요청 허용, JS 요청 허용
-        configuration.addAllowedOrigin("http://localhost:3000");
+        //TODO 추후 배포시 수정 필요
+        configuration.addAllowedOrigin("*");
         configuration.setAllowCredentials(true); // 클라이언트에서 쿠키 요청하는 것을 허용
         configuration.addExposedHeader("Authorization"); // Authorization 헤더를 노출
 
