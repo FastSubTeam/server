c31e2f3028de9599deefce0573d8b27944d894e5
brotherstone97 <brotherstone97@gmail.com>
2023-07-13 13:57:22 +0900

    [#33] feat: 페이징된 컨텐츠를 반환하는 repository 추상메서드 선언
    query parameter로 입력받은 page와 size를 기반으로 이에 해당하는 컨텐츠 반환

--- src/main/java/com/popple/server/domain/board/repository/BoardRepository.java
+++ src/main/java/com/popple/server/domain/board/repository/BoardRepository.java
@@ -1,7 +1,10 @@
 package com.popple.server.domain.board.repository;
 
 import com.popple.server.domain.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Post, Long> {
+    Page<Post> findAll(Pageable pageable);
 }
