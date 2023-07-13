c50ad701763c0cd420538f7b7fae31ada859fb16
brotherstone97 <brotherstone97@gmail.com>
2023-07-13 16:45:13 +0900

    feat: [#33] 페이징된 게시물을 응답하는 컨트롤러 구현

--- src/main/java/com/popple/server/domain/board/controller/BoardController.java
+++ src/main/java/com/popple/server/domain/board/controller/BoardController.java
@@ -7,6 +7,8 @@ import com.popple.server.domain.entity.Comment;
 import com.popple.server.domain.entity.Post;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +31,28 @@ public class BoardController {
         //서비스 메서드 호출
         List<Post> posts = boardService.getAllPosts();
         for (Post post : posts) {
+            List<Comment> comments = boardService.getAllCommentsByPostId(post.getId());
+            int commentCount = comments.size();
+            BoardListRespDto boardListRespDto = BoardListRespDto.builder()
+                    .id(post.getId())
+                    .nickname(post.getMember().getNickname())
+                    .title(post.getTitle())
+                    .content(post.getContent())
+                    .createdAt(post.getCreatedAt())
+                    .updatedAt(post.getUpdatedAt())
+                    .commentCount(commentCount)
+                    .build();
+            boardListRespDtoList.add(boardListRespDto);
+        }
+        return APIDataResponse.of(HttpStatus.OK, boardListRespDtoList);
+    }
+
+    @RequestMapping("/board")
+    public APIDataResponse<List<BoardListRespDto>> getPostsByPage(Pageable pageable) {
+        Page<Post> postsByPage = boardService.getPostsByPage(pageable);
+        List<Post> contents = postsByPage.getContent();
+        List<BoardListRespDto> boardListRespDtoList = new ArrayList<>();
+        for (Post post : contents) {
             List<Comment> comments = boardService.getAllCommentsByPostId(post.getId());
             int commentCount = comments.size();
             BoardListRespDto boardListRespDto = BoardListRespDto.builder()
