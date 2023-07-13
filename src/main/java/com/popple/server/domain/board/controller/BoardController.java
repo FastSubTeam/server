7087a518882beb217b4dd464228c892abd423f9b
brotherstone97 <brotherstone97@gmail.com>
2023-07-13 18:02:09 +0900

    [#33] feat: 컨트롤러 메서드에 @PageableDefault 적용
    요청의 편의성을 위해 query parameter로 page를 보내지 않으면 0페이지를 응답하고
    size를 보내지 않으면 10을 기본으로 설정해주는 애노테이션 적용했습니다.

--- src/main/java/com/popple/server/domain/board/controller/BoardController.java
+++ src/main/java/com/popple/server/domain/board/controller/BoardController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,7 +35,7 @@ public class BoardController {
     }
 
     @RequestMapping("/board")
-    public BoardAPIDataResponse<List<BoardListRespDto>> getPostsByPage(Pageable pageable) {
+    public BoardAPIDataResponse<List<BoardListRespDto>> getPostsByPage(@PageableDefault Pageable pageable) {
         Page<Post> postsByPage = boardService.getPostsByPage(pageable);
         List<Post> contents = postsByPage.getContent();
         List<BoardListRespDto> boardListRespDtoList = createListOfBoardListRespDto(contents);
