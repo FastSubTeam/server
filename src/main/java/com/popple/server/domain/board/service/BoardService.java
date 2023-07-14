e34fa14003810ec0af8c2f4bb27a276396ec8a51
brotherstone97 <brotherstone97@gmail.com>
2023-07-14 21:13:23 +0900

    [#40] fix: postId 기반의 List<Comment>리턴 -> postId 기반의 List<CommentDTO>리턴
    Comment테이블의 특정 컬럼만 프로젝션하는 것으로 변경됨에 따라
    postId를 통해 모든 댓글을 찾던 메서드의 리턴타입도 다음과 같이 변경했습니다.
    List<Comment> -> List<CommentDTO>

--- src/main/java/com/popple/server/domain/board/service/BoardService.java
+++ src/main/java/com/popple/server/domain/board/service/BoardService.java
@@ -1,9 +1,12 @@
 package com.popple.server.domain.board.service;
 
+import com.popple.server.domain.board.dto.CommentDto;
+import com.popple.server.domain.board.dto.CommentTableProjection;
 import com.popple.server.domain.board.repository.BoardRepository;
 import com.popple.server.domain.board.repository.CommentRepository;
-import com.popple.server.domain.entity.Comment;
+import com.popple.server.domain.entity.Member;
 import com.popple.server.domain.entity.Post;
+import com.popple.server.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -11,6 +14,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -22,13 +26,39 @@ import java.util.Optional;
 public class BoardService {
     private final BoardRepository boardRepository;
     private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
 
     public List<Post> getAllPosts() {
         return boardRepository.findAll();
     }
 
-    public List<Comment> getAllCommentsByPostId(Long postId) {
-        return commentRepository.findByPost_Id(postId);
+    public List<CommentDto> getAllCommentsByPostId(Long postId) {
+        List<CommentTableProjection> byPost_id = commentRepository.findByPost_id(postId);
+        List<CommentDto> commentDtos = new ArrayList<>();
+        for (CommentTableProjection c : byPost_id) {
+            Optional<Member> member = userRepository.findById(c.getMemberId());
+            if (member.isPresent()) {
+                CommentDto commentDto = CommentDto.builder()
+                        .id(c.getId())
+                        .content(c.getContent())
+                        .createdAt(c.getCreatedAt())
+                        .updatedAt(c.getUpdatedAt())
+                        .member(member.get())
+                        .build();
+                commentDtos.add(commentDto);
+                continue;
+            }
+            CommentDto commentDto = CommentDto.builder()
+                    .id(c.getId())
+                    .content(c.getContent())
+                    .createdAt(c.getCreatedAt())
+                    .updatedAt(c.getUpdatedAt())
+                    .member(null)// Todo 존재하지 않는 사용자에 대한 예외처리
+                    .build();
+            commentDtos.add(commentDto);
+            log.error("comment_id = {}의 Member정보가 존재하지 않습니다.", c.getId());
+        }
+        return commentDtos;
     }
 
     public Page<Post> getPostsByPage(Pageable pageable) {
@@ -36,9 +66,9 @@ public class BoardService {
         return boardRepository.findAll(pageable);
     }
 
-    public Post getPostById(Long postId){
+    public Post getPostById(Long postId) {
         Optional<Post> post = boardRepository.findById(postId);
-        if(post.isPresent()){
+        if (post.isPresent()) {
             return post.get();
         }
         throw new NoSuchElementException("게시물의 id가 존재하지 않습니다.");
