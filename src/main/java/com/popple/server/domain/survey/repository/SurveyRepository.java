74e098ec821c65f0afd9611ea263e217ad88b8b5
khsrla9806 <kimhunsope@naver.com>
2023-08-02 00:10:59 +0900

    [#54] feat: 12시 정각에 실행되는 수요조사 스케쥴러 구현
    - 00:00에 작동되는 것을 직접 확인함
    - startDate와 endDate를 확인해서 12시 정각에 수요조사의 상태를 자동으로 바꾸도록 설정

--- src/main/java/com/popple/server/domain/survey/repository/SurveyRepository.java
+++ src/main/java/com/popple/server/domain/survey/repository/SurveyRepository.java
@@ -1,7 +1,11 @@
 package com.popple.server.domain.survey.repository;
 
 import com.popple.server.domain.entity.Survey;
+import com.popple.server.domain.survey.type.SurveyStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SurveyRepository extends JpaRepository<Survey, Integer> {
+    List<Survey> findAllByStatusIsNot(SurveyStatus status);
 }
