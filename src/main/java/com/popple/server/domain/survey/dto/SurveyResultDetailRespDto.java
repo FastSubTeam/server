fa794c38087ad1aa2444588f397e7e1dd41bca69
khsrla9806 <kimhunsope@naver.com>
2023-08-02 20:42:16 +0900

    [#55] feat: 수요조사 결과 상세 조회에 필요한 DTO 구현
    - 응답 결과는 survey_result와 member의 join 결과를 받기 위해 interface로 구현
    - member에서 area만 필요한데, 그외에 불필요한 SELECT 쿼리가 연속 작동
    - 그래서 join으로 member의 원하는 필드인 area만 Projection 하도록 구현

---
+++ src/main/java/com/popple/server/domain/survey/dto/SurveyResultDetailRespDto.java
@@ -0,0 +1,63 @@
+package com.popple.server.domain.survey.dto;
+
+import com.popple.server.domain.entity.Survey;
+import com.popple.server.domain.entity.SurveyOption;
+import com.popple.server.domain.entity.SurveyResult;
+import com.popple.server.domain.survey.type.SurveyStatus;
+import lombok.*;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Builder
+public class SurveyResultDetailRespDto {
+    private Integer id;
+    private String title;
+    private Timestamp startDate;
+    private Timestamp endDate;
+    private SurveyStatus status;
+    private List<OptionRespDto> options;
+    private List<SurveyAnswer> answers;
+
+    public static SurveyResultDetailRespDto fromEntity(
+            Survey survey, List<SurveyOption> options, List<SurveyAnswer> answers) {
+        List<OptionRespDto> convertedOptions = options.stream()
+                .map(OptionRespDto::fromEntity).collect(Collectors.toList());
+
+        return SurveyResultDetailRespDto.builder()
+                .id(survey.getId())
+                .title(survey.getTitle())
+                .startDate(survey.getStartDate())
+                .endDate(survey.getEndDate())
+                .status(survey.getStatus())
+                .options(convertedOptions)
+                .answers(answers)
+                .build();
+    }
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    @Builder
+    static class OptionRespDto {
+        private Integer id;
+        private String content;
+
+        public static OptionRespDto fromEntity(SurveyOption option) {
+            return OptionRespDto.builder()
+                    .id(option.getId())
+                    .content(option.getContent())
+                    .build();
+        }
+    }
+
+    public interface SurveyAnswer {
+        Integer getAnswerId();
+        Long getUserId();
+        Integer getOptionId();
+        Integer getAge();
+        String getArea();
+    }
+}
