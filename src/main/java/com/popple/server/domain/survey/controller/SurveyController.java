a5fb59c00b66a7fc0d91b6387f28efee109636d7
khsrla9806 <kimhunsope@naver.com>
2023-08-02 18:22:28 +0900

    [#55] feat: 수요조사 응답 제출 Controller 메서드 구현
    - 기존에 사용하던 Authentication 대신 @LoginActor와 Actor로 수정

--- src/main/java/com/popple/server/domain/survey/controller/SurveyController.java
+++ src/main/java/com/popple/server/domain/survey/controller/SurveyController.java
@@ -1,12 +1,12 @@
 package com.popple.server.domain.survey.controller;
 
 import com.popple.server.common.dto.APIDataResponse;
+import com.popple.server.domain.survey.dto.SurveyResultCreateReqDto;
 import com.popple.server.domain.survey.service.SurveyService;
+import com.popple.server.domain.user.annotation.LoginActor;
+import com.popple.server.domain.user.vo.Actor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,12 +16,18 @@ public class SurveyController {
     private final SurveyService surveyService;
 
     @GetMapping("/active")
-    public APIDataResponse<?> getActiveSurvey(Authentication authentication) {
-        return this.surveyService.findActiveSurvey(authentication);
+    public APIDataResponse<?> getActiveSurvey(@LoginActor Actor loginMember) {
+        return this.surveyService.findActiveSurvey(loginMember);
     }
 
     @GetMapping("/result")
     public APIDataResponse<?> getSurveyResults() {
-        return surveyService.getSurveyResults();
+        return this.surveyService.getSurveyResults();
+    }
+
+    @PostMapping
+    public APIDataResponse<?> submitSurvey(
+            @RequestBody SurveyResultCreateReqDto dto, @LoginActor Actor loginMember) {
+        return this.surveyService.createSurveyResult(dto, loginMember);
     }
 }
