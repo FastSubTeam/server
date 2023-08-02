4567957b9bcc88a9c1d12722587d0635519956a9
khsrla9806 <kimhunsope@naver.com>
2023-08-02 15:53:44 +0900

    [#55] feat: 현재 진행중인 수요조사 정보 조회 기능 구현
    (SurveyController) 요청을 받기 위한 Controller 메서드 구현
    (ActiveSurveyRespDto) 진행중인 수요조사 응답 데이터를 위한 DTO 구현
    (SurveyRepository) status로 Survey 정보 가져오는 쿼리 메서드 구현 (startDate 정렬 후 First)
    (SurveyResultRepository) member, survey로 수요조사에 참여했는지 찾는 쿼리 메서드 구현
    (SurveyService) 진행중인 수요조사의 로직을 담은 Service 메서드 구현

---
+++ src/main/java/com/popple/server/domain/survey/controller/SurveyController.java
@@ -0,0 +1,22 @@
+package com.popple.server.domain.survey.controller;
+
+import com.popple.server.common.dto.APIDataResponse;
+import com.popple.server.domain.survey.service.SurveyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/survey")
+public class SurveyController {
+
+    private final SurveyService surveyService;
+
+    @GetMapping("/active")
+    public APIDataResponse<?> getActiveSurvey(Authentication authentication) {
+        return this.surveyService.findActiveSurvey(authentication);
+    }
+}
