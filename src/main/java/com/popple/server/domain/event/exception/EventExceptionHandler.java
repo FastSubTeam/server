8663dd75d862393d6549640315d99e169210f36c
khsrla9806 <kimhunsope@naver.com>
2023-08-18 16:54:24 +0900

    [#65] feat: Event 커스텀 예외처리 클래스와 ExceptionHandler 구현
    - 예외 메시지도 인터페이스로 상수화

---
+++ src/main/java/com/popple/server/domain/event/exception/EventExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.popple.server.domain.event.exception;
+
+import com.popple.server.common.dto.APIErrorResponse;
+import com.popple.server.domain.event.controller.EventController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(basePackageClasses = { EventController.class })
+public class EventExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler
+    public APIErrorResponse eventExceptionHandler(EventException exception) {
+        return APIErrorResponse.of(HttpStatus.BAD_REQUEST, exception.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler
+    public APIErrorResponse exceptionHandler(Exception exception) {
+        return APIErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
+    }
+}
