47c1a61fdf2099446a2b082b2a567c6843c13b99
khsrla9806 <kimhunsope@naver.com>
2023-08-18 16:52:17 +0900

    [#65] feat: 행사 생성, 목록 조회, 상세 조회 DTO 구현

--- src/main/java/com/popple/server/domain/event/dto/EventCreateReqDto.java
+++ src/main/java/com/popple/server/domain/event/dto/EventCreateReqDto.java
@@ -1,25 +1,57 @@
 package com.popple.server.domain.event.dto;
 
-
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.popple.server.domain.entity.Event;
 import com.popple.server.domain.entity.Seller;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import com.popple.server.domain.event.EventApproval;
+import com.popple.server.domain.event.EventStatus;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
-@AllArgsConstructor
 @NoArgsConstructor
-@Getter
 @Setter
 public class EventCreateReqDto {
     private String name;
     private String description;
     private String location;
+    private String category;
     private String thumbnailUrl;
-    private Timestamp startDate;
-    private Timestamp endDate;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
 
+    public Event toEntity(Seller seller) {
+        return Event.builder()
+                .name(this.name)
+                .description(this.description)
+                .location(this.location)
+                .category(this.category)
+                .thumbnailUrl(this.thumbnailUrl)
+                .startDate(toTimestamp(this.startDate))
+                .endDate(toTimestamp(this.endDate))
+                .approval(EventApproval.WAIT)
+                .status(getCurrentStatus())
+                .host(seller)
+                .build();
+    }
+
+    private Timestamp toTimestamp(LocalDateTime localDateTime) {
+        return Timestamp.valueOf(localDateTime);
+    }
+
+    private EventStatus getCurrentStatus() {
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(this.endDate);
+        System.out.println(now);
+        if (this.endDate.isBefore(now)) {
+            return EventStatus.END;
+        }
+
+        if (this.endDate.isAfter(now) && this.startDate.isBefore(now)) {
+            return EventStatus.PROCEEDING;
+        }
+
+        return EventStatus.WAIT;
+    }
 }
