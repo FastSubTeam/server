3ce6100bb3670d32f340fbadf5858faa18efe777
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-08-18 20:40:39 +0900

    [#67] chore: 이메일 인증 API 수정
    Redirect 삭제

--- src/main/java/com/popple/server/domain/user/exception/UserErrorCode.java
+++ src/main/java/com/popple/server/domain/user/exception/UserErrorCode.java
@@ -12,7 +12,7 @@ public enum UserErrorCode implements ErrorCode {
     PROCEEDING_EMAIL(HttpStatus.BAD_REQUEST, "이미 회원가입을 진행중인 이메일입니다"),
     NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 회원입니다"),
     INVALID_CHECK_DUPLICATION_PARAMETER(HttpStatus.BAD_REQUEST, "중복 확인을 하려는 파라미터를 입력해주세요"),
-    ;
+    NOT_FOUND_REGISTER_TOKEN(HttpStatus.BAD_REQUEST, "유효하지 않은 인증토큰입니다.");
 
     private final HttpStatus httpStatus;
     private final String errorMessage;
