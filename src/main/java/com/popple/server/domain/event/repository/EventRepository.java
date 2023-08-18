97497770f3823e207b1b8fe35be7a3f2e1554baa
khsrla9806 <kimhunsope@naver.com>
2023-08-18 16:56:01 +0900

    [#65] feat: 행사 정보와 관련된 개인 판매자(host)를 가져오기 위한 쿼리 메서드 구현
    - host와 Join Fetch로 가져오는 쿼리 메서드 구현

--- src/main/java/com/popple/server/domain/event/repository/EventRepository.java
+++ src/main/java/com/popple/server/domain/event/repository/EventRepository.java
@@ -4,8 +4,15 @@ import com.popple.server.domain.entity.Event;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 
 public interface EventRepository extends JpaRepository<Event, Long> {
     Page<Event> findAll(Pageable pageable);
+
+    @Query("select m from Event m left join fetch m.host where m.id = :id")
+    Optional<Event> findEventByIdJoinFetchSeller(@Param("id") Long id);
 }
