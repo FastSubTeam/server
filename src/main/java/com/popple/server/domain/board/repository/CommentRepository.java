06167a1aa69cf79f6f9f2d6d188453415a3e2db8
brotherstone97 <brotherstone97@gmail.com>
2023-08-20 02:59:02 +0900

    [#73] feat: 특정 post의 모든 댓글을 삭제하는 Repository메서드 구현
    DB오류 방지를 위해 게시물 삭제 전 게시물의 모든 댓글을 삭제할 용도로 구현.

--- src/main/java/com/popple/server/domain/board/repository/CommentRepository.java
+++ src/main/java/com/popple/server/domain/board/repository/CommentRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<CommentTableProjection> findByPost_id(Long postId);
+    void deleteAllByPost_id(Long postId);
 }
