a76e8438412ce5d656954d34420f9c3b61b44c18
khsrla9806 <kimhunsope@naver.com>
2023-08-21 19:49:41 +0900

    [#88] feat: Event 수정 Service 로직 구현 및 Exception 메시지 추가

--- src/main/java/com/popple/server/domain/event/exception/EventExceptionMessage.java
+++ src/main/java/com/popple/server/domain/event/exception/EventExceptionMessage.java
@@ -4,4 +4,6 @@ public interface EventExceptionMessage {
     String NON_EXIST_EVENT = "존재 하지 않는 행사 정보입니다.";
     String NON_EXIST_SELLER = "존재하지 않는 개인 판매자 정보입니다.";
     String NONE_VALID_LOGIN_SELLER = "유저가 존재하지 않거나, 개인 판매자가 아닌 유저 정보입니다.";
+    String NOT_MATCH_OWNER_OF_EVENT = "해당 행사 정보를 작성한 판매자가 아닙니다.";
+    String FAIL_VALIDATION_CHECK = "유효성 검사에 실패했습니다.";
 }
