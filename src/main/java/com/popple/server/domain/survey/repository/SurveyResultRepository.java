13ab7cb73a7d37e5cce18ef29ee01b5788dd4385
khsrla9806 <kimhunsope@naver.com>
2023-08-22 17:05:16 +0900

    [#110] fix: 수요조사 쿼리 Where 조건 코드 수정

--- src/main/java/com/popple/server/domain/survey/repository/SurveyResultRepository.java
+++ src/main/java/com/popple/server/domain/survey/repository/SurveyResultRepository.java
@@ -6,15 +6,17 @@ import com.popple.server.domain.entity.SurveyResult;
 import com.popple.server.domain.survey.dto.SurveyResultDetailRespDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface SurveyResultRepository extends JpaRepository<SurveyResult, Integer> {
     Optional<SurveyResult> findByMemberAndSurvey(Member member, Survey survey);
+
     @Query("SELECT s.id AS answerId, m.id AS userId, s.surveyOption.id AS optionId, s.age AS age, m.city AS area " +
             "FROM SurveyResult s " +
-            "JOIN Member m " +
-            "ON s.member.id = m.id")
-    List<SurveyResultDetailRespDto.SurveyAnswer> findBySurveyId(Integer surveyId);
+            "JOIN s.member m " +
+            "WHERE s.survey.id = :surveyId")
+    List<SurveyResultDetailRespDto.SurveyAnswer> findBySurveyId(@Param("surveyId") Integer surveyId);
 }
