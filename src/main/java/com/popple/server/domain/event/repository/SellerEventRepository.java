252573ed5590ca2ea30f96b59d690f46b8a920a0
khsrla9806 <kimhunsope@naver.com>
2023-08-22 19:48:58 +0900

    [#113] feat: 행사 참여자 정보를 얻기 위한 쿼리 메서드 구현

--- src/main/java/com/popple/server/domain/event/repository/SellerEventRepository.java
+++ src/main/java/com/popple/server/domain/event/repository/SellerEventRepository.java
@@ -3,10 +3,24 @@ package com.popple.server.domain.event.repository;
 import com.popple.server.domain.entity.Event;
 import com.popple.server.domain.entity.Seller;
 import com.popple.server.domain.entity.SellerEvent;
+import com.popple.server.domain.event.dto.EventParticipantRepDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface SellerEventRepository extends JpaRepository<SellerEvent, Long> {
     void deleteAllByEventId(Long eventId);
     boolean existsByEventAndSeller(Event event, Seller seller);
     void deleteByEventAndSeller(Event event, Seller seller);
+
+    @Query("select new com.popple.server.domain.event.dto.EventParticipantRepDto(" +
+            "   se.seller.id, " +
+            "   se.seller.nickname, " +
+            "   se.seller.profileImgUrl" +
+            ") " +
+            "from SellerEvent se " +
+            "where se.event = :event")
+    List<EventParticipantRepDto> findParticipantsByEvent(@Param("event") Event event);
 }
