92c3da6b71707cf4b848df3bddf25b6d70704531
khsrla9806 <kimhunsope@naver.com>
2023-08-22 23:46:00 +0900

    [#118] feat: 행사 상태를 변경하는 벌크 update 쿼리 메서드 구현

--- src/main/java/com/popple/server/domain/event/repository/EventRepository.java
+++ src/main/java/com/popple/server/domain/event/repository/EventRepository.java
@@ -4,9 +4,12 @@ import com.popple.server.domain.entity.Event;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -15,4 +18,25 @@ public interface EventRepository extends JpaRepository<Event, Long> {
 
     @Query("select m from Event m left join fetch m.host where m.id = :id")
     Optional<Event> findEventByIdJoinFetchSeller(@Param("id") Long id);
+
+    @Modifying
+    @Query("update Event e " +
+            "set e.status = 'PROCEEDING' " +
+            "where e.status != 'PROCEEDING' " +
+            "and :now between substring(e.startDate, 1, 10) and substring(e.endDate, 1, 10) ")
+    void updateProceedingEvent(@Param("now") String now);
+
+    @Modifying
+    @Query("update Event e " +
+            "set e.status = 'WAIT' " +
+            "where e.status != 'WAIT' " +
+            "and :now < substring(e.startDate, 1, 10) ")
+    void updateWaitEvent(@Param("now") String now);
+
+    @Modifying
+    @Query("update Event e " +
+            "set e.status = 'END' " +
+            "where e.status != 'END' " +
+            "and :now > substring(e.endDate, 1, 10) ")
+    void updateEndEvent(@Param("now") String now);
 }
