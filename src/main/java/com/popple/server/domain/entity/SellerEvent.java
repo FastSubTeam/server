424c5e3137e045a6af71c76cb7438e68bcd3839f
khsrla9806 <kimhunsope@naver.com>
2023-08-22 18:23:54 +0900

    [#112] feat: 셀러 행사 관계 엔티티의 생성자 추가
    - seller, event 객체를 받아서 만드는 생성자 추가

--- src/main/java/com/popple/server/domain/entity/SellerEvent.java
+++ src/main/java/com/popple/server/domain/entity/SellerEvent.java
@@ -1,8 +1,11 @@
 package com.popple.server.domain.entity;
 
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
+@NoArgsConstructor
 @Table(
         name = "seller_event",
         uniqueConstraints =
@@ -12,6 +15,13 @@ import javax.persistence.*;
         )
 )
 public class SellerEvent extends BaseEntity {
+
+    public SellerEvent(Seller seller, Event event) {
+        this.seller = seller;
+        this.event = event;
+        this.approval = "APPROVAL"; // TODO: 승인, 반려 기능은 아직 넣을지 말지 확정 X
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
