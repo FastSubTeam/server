8966c02aa37b573a3bd598c93d8e7b7d7737d041
khsrla9806 <kimhunsope@naver.com>
2023-08-22 18:28:17 +0900

    [#112] feat: event, seller로 이미 참여하고 있는 행사인지 확인하는 메서드 구현

--- src/main/java/com/popple/server/domain/event/repository/SellerEventRepository.java
+++ src/main/java/com/popple/server/domain/event/repository/SellerEventRepository.java
@@ -1,9 +1,11 @@
 package com.popple.server.domain.event.repository;
 
 import com.popple.server.domain.entity.Event;
+import com.popple.server.domain.entity.Seller;
 import com.popple.server.domain.entity.SellerEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SellerEventRepository extends JpaRepository<SellerEvent, Long> {
     void deleteAllByEventId(Long eventId);
+    boolean existsByEventAndSeller(Event event, Seller seller);
 }
