42c1bd3d2bddd45f3c7543e12508043af3493968
khsrla9806 <kimhunsope@naver.com>
2023-08-22 18:37:08 +0900

    [#112] feat: 행사 참여 신청, 삭제에 대한 예외 메시지 상수화

--- src/main/java/com/popple/server/domain/event/exception/EventExceptionMessage.java
+++ src/main/java/com/popple/server/domain/event/exception/EventExceptionMessage.java
@@ -6,4 +6,6 @@ public interface EventExceptionMessage {
     String NONE_VALID_LOGIN_SELLER = "유저가 존재하지 않거나, 개인 판매자가 아닌 유저 정보입니다.";
     String NOT_MATCH_OWNER_OF_EVENT = "해당 행사 정보를 작성한 판매자가 아닙니다.";
     String FAIL_VALIDATION_CHECK = "유효성 검사에 실패했습니다.";
+    String ALREADY_JOIN_EVENT = "이미 해당 행사에 참여하는 개인 판매자입니다.";
+    String NOT_JOIN_EVENT = "해당 행사에 참여하고 있지 않은 개인 판매자입니다.";
 }
