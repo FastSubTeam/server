963b0ede1cf6b5ce233954f12fbe411b4629f150
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-08-22 19:48:29 +0900

    [#115] feat: 메인 로직 구현

--- src/main/java/com/popple/server/domain/user/dto/CreateSellerRequestDto.java
+++ src/main/java/com/popple/server/domain/user/dto/CreateSellerRequestDto.java
@@ -3,9 +3,9 @@ package com.popple.server.domain.user.dto;
 import lombok.*;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
-import java.util.List;
 
 @Getter
 @Builder
@@ -17,24 +17,27 @@ public class CreateSellerRequestDto {
 
     @Email
     @NotNull
+    @NotEmpty
     private String email;
 
     @Pattern(regexp = PASSWORD_REGEX, message = "비밀번호는 최소8글자, 최소 숫자1개, 문자1개를 포함하며 8글자 이상이어야 합니다.")
     @NotNull
+    @NotEmpty
     private String password;
 
     @NotNull
+    @NotEmpty
     private String address;
 
     @NotNull
+    @NotEmpty
     private String nickname;
 
     @NotNull
+    @NotEmpty
     private String shopName;
 
     @NotNull
+    @NotEmpty
     private String businessNumber;
-
-    @NotNull
-    private List<Long> categories;
 }
