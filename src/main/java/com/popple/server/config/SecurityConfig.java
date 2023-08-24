f8a0d381aebaa25b6e70c5feb67ce26b5ffc95b0
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-08-24 21:20:58 +0900

    [#120] feat: 로그인 사용자 프로필 조회

--- src/main/java/com/popple/server/config/SecurityConfig.java
+++ src/main/java/com/popple/server/config/SecurityConfig.java
@@ -96,8 +96,9 @@ public class SecurityConfig {
         httpSecurity.httpBasic().disable(); // 로그인 인증창이 뜨지 않도록 비활성화
         httpSecurity.authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/api/events/**", "/api/events", "/api/board").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/profile/seller").hasRole("SELLER")
                 .antMatchers(HttpMethod.POST, "/api/events").hasRole("SELLER")
-                .antMatchers(HttpMethod.PUT, "/api/events/**").hasRole("SELLER")
+                .antMatchers(HttpMethod.PUT, "/api/events/**", "/api/profile/seller").hasRole("SELLER")
                 .antMatchers(HttpMethod.DELETE, "/api/events/**").hasRole("SELLER")
                 .antMatchers(permitUserUrls).hasRole("USER")
                 .antMatchers(permitAllUrls).permitAll()
