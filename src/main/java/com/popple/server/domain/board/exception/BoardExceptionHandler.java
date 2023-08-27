b8b92e0420a5007f9025e9aa999faf713f80e803
brotherstone97 <brotherstone97@gmail.com>
2023-08-28 00:12:22 +0900

    [#136] feat: pricipal의 유저정보와 게시물 또는 댓글의 작성자가 다를 경우에 대한 예외 처리

---
+++ src/main/java/com/popple/server/domain/board/exception/BoardExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.popple.server.domain.board.exception;
+
+import com.popple.server.common.dto.APIErrorResponse;
+import com.popple.server.domain.board.controller.BoardController;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(basePackageClasses= BoardController.class)
+public class BoardExceptionHandler {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public APIErrorResponse handleUnAuthorizedException(UnauthorizedAccessException e){
+        return APIErrorResponse.of(HttpStatus.FORBIDDEN, e.getMessage());
+    }
+}
