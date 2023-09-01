61b75f12d9b0a3ace8c56ba88921625a9b34a448
khsrla9806 <kimhunsope@naver.com>
2023-09-02 00:03:26 +0900

    feat: random Token 확인용 로그 출력

--- src/main/java/com/popple/server/common/controller/CertificationController.java
+++ src/main/java/com/popple/server/common/controller/CertificationController.java
@@ -1,5 +1,6 @@
 package com.popple.server.common.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,12 +10,14 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+@Slf4j
 @Controller
 public class CertificationController {
 
     @GetMapping("/.well-known/acme-challenge/{randomToken}")
     @ResponseBody
     public String serveChallenge(@PathVariable String randomToken) {
+        log.info(">> Random Token {}", randomToken);
         return randomToken;
     }
 }
