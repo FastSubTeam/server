bf3a75f8b8817881a80141c870ac800203a6f349
khsrla9806 <kimhunsope@naver.com>
2023-09-01 23:43:49 +0900

    refactor: Certificate Controller 파일 생성 코드 제거

--- src/main/java/com/popple/server/common/controller/CertificationController.java
+++ src/main/java/com/popple/server/common/controller/CertificationController.java
@@ -15,17 +15,6 @@ public class CertificationController {
     @GetMapping("/.well-known/acme-challenge/{randomToken}")
     @ResponseBody
     public String serveChallenge(@PathVariable String randomToken) {
-        // randomToken 값을 파일 내용으로 사용하여 파일을 생성하고 반환합니다.
-        String challengeFilePath = "/var/www/letsencrypt/" + randomToken; // 파일 경로
-        File challengeFile = new File(challengeFilePath);
-        try {
-            FileWriter fileWriter = new FileWriter(challengeFile);
-            fileWriter.write(randomToken);
-            fileWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return e.getMessage();
-        }
         return randomToken;
     }
 }
