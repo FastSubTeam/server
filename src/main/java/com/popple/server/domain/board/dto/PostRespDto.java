0983b323e9efd322e528fcb8e1c36c38399e788f
brotherstone97 <brotherstone97@gmail.com>
2023-09-01 15:56:41 +0900

    [#139] chore: 게시물 데이터에 email정보도 포함되도록 수정

--- src/main/java/com/popple/server/domain/board/dto/PostRespDto.java
+++ src/main/java/com/popple/server/domain/board/dto/PostRespDto.java
@@ -14,6 +14,7 @@ import java.util.List;
 @ToString
 public class PostRespDto {
     private Long id;
+    private String email;
     private String nickname;
     private String title;
     private String content;
